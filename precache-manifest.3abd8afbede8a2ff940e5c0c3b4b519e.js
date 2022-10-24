7a664190c0500e433f0fc66cc76b9ec296a40767
Gary Welch <welch.gary@gmail.com>
2022-10-23 21:24:11 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.3abd8afbede8a2ff940e5c0c3b4b519e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "d35ed5857a3653d5b366",
+    "url": "/pgoWeakness/static/css/main.3d7a235b.chunk.css"
+  },
+  {
+    "revision": "d35ed5857a3653d5b366",
+    "url": "/pgoWeakness/static/js/main.677dafe8.chunk.js"
+  },
+  {
+    "revision": "076ced8d23433160ec87",
+    "url": "/pgoWeakness/static/js/runtime~main.650c4638.js"
+  },
+  {
+    "revision": "f684e6e88b09fb989699",
+    "url": "/pgoWeakness/static/css/2.d11923f7.chunk.css"
+  },
+  {
+    "revision": "f684e6e88b09fb989699",
+    "url": "/pgoWeakness/static/js/2.84cc74a9.chunk.js"
+  },
+  {
+    "revision": "433f60a63be1d2d5bbba",
+    "url": "/pgoWeakness/static/js/3.91493895.chunk.js"
+  },
+  {
+    "revision": "14540979a6685ab378bf0cee1e97f942",
+    "url": "/pgoWeakness/static/media/bug.14540979.svg"
+  },
+  {
+    "revision": "b7d4f57872507c79b1ecd192aad61357",
+    "url": "/pgoWeakness/static/media/dark.b7d4f578.svg"
+  },
+  {
+    "revision": "18a1dbc9e3a69ba77e890268451026e4",
+    "url": "/pgoWeakness/static/media/dragon.18a1dbc9.svg"
+  },
+  {
+    "revision": "68b142da814c55f9225db2c16193ba01",
+    "url": "/pgoWeakness/static/media/electric.68b142da.svg"
+  },
+  {
+    "revision": "603e08d1937f16395d2d304ef6d30ab7",
+    "url": "/pgoWeakness/static/media/fairy.603e08d1.svg"
+  },
+  {
+    "revision": "8ce4fa81f8f29f8c6871b88f78916637",
+    "url": "/pgoWeakness/static/media/fighting.8ce4fa81.svg"
+  },
+  {
+    "revision": "33d14b307cd671ab9c1ca76f4788df7a",
+    "url": "/pgoWeakness/static/media/fire.33d14b30.svg"
+  },
+  {
+    "revision": "8c1e094fa4fa86cf9a4b943d6b6c9253",
+    "url": "/pgoWeakness/static/media/flying.8c1e094f.svg"
+  },
+  {
+    "revision": "ff3973459922626c46b8935bd3517deb",
+    "url": "/pgoWeakness/static/media/ghost.ff397345.svg"
+  },
+  {
+    "revision": "d589dd00e175efada7294faf05f52ac6",
+    "url": "/pgoWeakness/static/media/grass.d589dd00.svg"
+  },
+  {
+    "revision": "289c4927e264d15f5be41d5c53f5c601",
+    "url": "/pgoWeakness/static/media/ground.289c4927.svg"
+  },
+  {
+    "revision": "f82b7e5d85efb43a4e5001ed490269d4",
+    "url": "/pgoWeakness/static/media/ice.f82b7e5d.svg"
+  },
+  {
+    "revision": "8f2a7aaa9ae7bd49980f1b2b99a2577f",
+    "url": "/pgoWeakness/static/media/normal.8f2a7aaa.svg"
+  },
+  {
+    "revision": "183135e96caba5df8c380e0d2acdea6a",
+    "url": "/pgoWeakness/static/media/poison.183135e9.svg"
+  },
+  {
+    "revision": "60982f05e13fbd9b6e6d96ce3456d365",
+    "url": "/pgoWeakness/static/media/psychic.60982f05.svg"
+  },
+  {
+    "revision": "499a0fa10362807e9a3218082be3d288",
+    "url": "/pgoWeakness/static/media/rock.499a0fa1.svg"
+  },
+  {
+    "revision": "95342da3d89b3644b12d596a41d631c7",
+    "url": "/pgoWeakness/static/media/steel.95342da3.svg"
+  },
+  {
+    "revision": "e29fb8e379f9ed5335a02eb3881d25d6",
+    "url": "/pgoWeakness/static/media/water.e29fb8e3.svg"
+  },
+  {
+    "revision": "461c468ffd1fb2711dd45d97589ed70b",
+    "url": "/pgoWeakness/index.html"
+  }
+];
